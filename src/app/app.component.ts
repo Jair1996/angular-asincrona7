0f3130da924f470d5f45fd0e685a26a51e6bc1ed
jair <jairdeyner730791@gmail.com>
2022-12-29 10:48:24 -0500

    Estilo y lógica agregada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'asincrona7';
+  numero: number = 0;
+  openAlert: boolean = false;
+
+  lanzarDado() {
+    this.openAlert = false;
+    const valor = this.getRandomNumber(1, 6);
+
+    if (valor === this.numero) {
+      this.openAlert = true;
+      return;
+    }
+
+    this.numero = valor;
+  }
+
+  private getRandomNumber(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }
