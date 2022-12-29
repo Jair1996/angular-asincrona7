da3a1dde2c898c4576fe8fe9d0372c1e181ed5ed
jair <jairdeyner730791@gmail.com>
2022-12-29 11:03:53 -0500

    Animación rotate-scale-down agregada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,19 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('btnDado') btnDado!: any;
+
   numero: number = 0;
   openAlert: boolean = false;
 
   lanzarDado() {
+    this.btnDado.nativeElement.classList.add('rotate-scale-down');
     this.openAlert = false;
+
     const valor = this.getRandomNumber(1, 6);
 
     if (valor === this.numero) {
       this.openAlert = true;
+      this.btnDado.nativeElement.classList.remove('rotate-scale-down');
       return;
     }
 
-    this.numero = valor;
+    setTimeout(() => {
+      this.numero = valor;
+      this.btnDado.nativeElement.classList.remove('rotate-scale-down');
+    }, 1000);
   }
 
   private getRandomNumber(min: number, max: number) {
